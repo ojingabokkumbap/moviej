6011245cfd29e6d1273f0268197fd50f192e94fc
ojingabokkumbap <yooheez@naver.com>
2025-10-02 14:33:45 +0900

    svg 속성명 class -> className 변경

--- moviej-frontend/src/app/onboarding/Step3Movies.tsx
+++ moviej-frontend/src/app/onboarding/Step3Movies.tsx
@@ -340,7 +340,7 @@ export default function Step3Movies({
                               height="17"
                               viewBox="0 0 24 24"
                               fill="currentColor"
-                              class="icon icon-tabler icons-tabler-filled icon-tabler-star"
+                              className="icon icon-tabler icons-tabler-filled icon-tabler-star"
                             >
                               <path
                                 stroke="none"
@@ -396,7 +396,7 @@ export default function Step3Movies({
                     strokeWidth="2"
                     strokeLinecap="round"
                     strokeLinejoin="round"
-                    class="icon icon-tabler icons-tabler-outline icon-tabler-x"
+                    className="icon icon-tabler icons-tabler-outline icon-tabler-x"
                   >
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M18 6l-12 12" />
@@ -412,7 +412,7 @@ export default function Step3Movies({
                       height="14"
                       viewBox="0 0 24 24"
                       fill="currentColor"
-                      class="icon icon-tabler icons-tabler-filled icon-tabler-star"
+                      className="icon icon-tabler icons-tabler-filled icon-tabler-star"
                     >
                       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                       <path d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
@@ -441,7 +441,7 @@ export default function Step3Movies({
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            class="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"
+            className="icon icon-tabler icons-tabler-outline icon-tabler-chevron-left"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M15 6l-6 6l6 6" />
@@ -458,7 +458,7 @@ export default function Step3Movies({
                 height="24"
                 viewBox="0 0 24 24"
                 fill="currentColor"
-                class="icon icon-tabler icons-tabler-filled icon-tabler-alert-circle"
+                className="icon icon-tabler icons-tabler-filled icon-tabler-alert-circle"
               >
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                 <path d="M12 2c5.523 0 10 4.477 10 10a10 10 0 0 1 -19.995 .324l-.005 -.324l.004 -.28c.148 -5.393 4.566 -9.72 9.996 -9.72zm.01 13l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 -8a1 1 0 0 0 -.993 .883l-.007 .117v4l.007 .117a1 1 0 0 0 1.986 0l.007 -.117v-4l-.007 -.117a1 1 0 0 0 -.993 -.883z" />
@@ -475,7 +475,7 @@ export default function Step3Movies({
                   height="24"
                   viewBox="0 0 24 24"
                   fill="currentColor"
-                  class="icon icon-tabler icons-tabler-filled icon-tabler-alert-circle"
+                  className="icon icon-tabler icons-tabler-filled icon-tabler-alert-circle"
                 >
                   <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                   <path d="M12 2c5.523 0 10 4.477 10 10a10 10 0 0 1 -19.995 .324l-.005 -.324l.004 -.28c.148 -5.393 4.566 -9.72 9.996 -9.72zm.01 13l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 -8a1 1 0 0 0 -.993 .883l-.007 .117v4l.007 .117a1 1 0 0 0 1.986 0l.007 -.117v-4l-.007 -.117a1 1 0 0 0 -.993 -.883z" />
